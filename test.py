c9880148170c5e8a9ab65628d5b44dcb61024359
justincdavis <davis.justin@mssm.org>
2023-11-19 19:19:38 -0700

    Testing tracing module calls

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 import sys
 import time
+import functools
 from typing import Any, Callable
 
 import torch
@@ -21,26 +22,27 @@ def get_traced_module(name: str, tracer_func: Callable[[Callable], Callable]) ->
     return in_mod
 
 
+def tracer_func(func: Callable) -> Callable:
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__}")
+        return func(*args, **kwargs)
+    return wrapper
+
 # make a decorate which takes a function and prints the string provided and then prints when function decorated is called
 def tracer(name: str):
-    def tracer_func(func):
-        def wrapper(*args, **kwargs):
-            print(f"Calling {name}.{func.__name__}")
-            return func(*args, **kwargs)
-        return wrapper
-    # replace module with a version which prints whenever a function is called
-    
     def decorator(func):
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            global _GLOBAL_STATE
             funcname = func.__name__
             in_args = args
             in_kwargs = kwargs
             og_mod = sys.modules[name]
-            traced_mod = get_traced_module(name)
+            traced_mod = get_traced_module(name, tracer_func=tracer_func)
             sys.modules[name] = traced_mod
             result = func(*args, **kwargs)
             sys.modules[name] = og_mod
+            global _GLOBAL_STATE
             _GLOBAL_STATE.append((time.time(), funcname, (in_args, in_kwargs), result))
             return result
         return wrapper
@@ -51,18 +53,6 @@ def test(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     return torch.add(a, b)
 
 def main():    
-    # create a function to trace input and outputs
-    def tracer(func):
-        def wrapper(*args, **kwargs):
-            global _GLOBAL_STATE
-            funcname = func.__name__
-            in_args = args
-            in_kwargs = kwargs
-            result = func(*args, **kwargs)
-            _GLOBAL_STATE.append((time.time(), funcname, (in_args, in_kwargs), result))
-            return result
-        return wrapper
-    
     test(torch.Tensor(1), torch.Tensor(2))
 
     print("BREAK")
